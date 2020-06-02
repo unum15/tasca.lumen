c86ae1dffd4574f7b44d1fa688a9f8949c0c5cd9
Kyle Waters <unum@unum5.org>
2020-06-02 06:52:32 -0600

    reauth

--- app/Http/Controllers/BackflowAssemblyController.php
+++ app/Http/Controllers/BackflowAssemblyController.php
@@ -10,7 +10,7 @@ class BackflowAssemblyController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     public function index(Request $request)
