0c0c24f81e2c054b89baace1c5ca2298ad74fc62
Kyle Waters <unum@unum5.org>
2020-12-28 09:14:02 -0700

    include categorsie for children

--- app/Http/Controllers/OverheadAssignmentController.php
+++ app/Http/Controllers/OverheadAssignmentController.php
@@ -16,7 +16,13 @@ class OverheadAssignmentController extends Controller
     {
         $includes = $this->validateIncludes($request->input('includes'));
         $values = $this->validateModel($request);
-        $items_query = OverheadAssignment::whereNull('parent_id')->with('overhead_categories')->with('children')->with('children.children')->with('children.children.children')->with('children.children.children.children');
+        $items_query = OverheadAssignment::whereNull('parent_id')
+            ->with('overhead_categories')
+            ->with('children')
+            ->with('children.overhead_categories')
+            ->with('children.children')
+            ->with('children.children.children')
+            ->with('children.children.children.children');
         foreach($values as $field => $value){
             $items_query->where($field, $value);
         }
