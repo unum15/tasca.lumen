1eac5290fbef526c9df85458e08ad9e5d3c573fd
Kyle Waters <unum@unum5.org>
2021-03-07 11:39:31 -0700

    add verify number procedure

---
+++ database/migrations/2021_03_07_105715_asset_number_check.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AssetNumberCheck extends Migration
+{
+    public function up()
+    {
+        $sql = "
+    CREATE FUNCTION verify_asset_number() 
+    RETURNS TRIGGER 
+    LANGUAGE PLPGSQL
+    AS $$
+    DECLARE
+        sub CHARACTER;
+        conflict INTEGER;
+    BEGIN
+        IF (NEW.item_number != '0') THEN
+        SELECT number INTO sub FROM asset_subs WHERE id = NEW.asset_sub_id;
+            IF (sub != '0') THEN
+                SELECT id INTO conflict FROM assets WHERE asset_sub_id = NEW.asset_sub_id AND item_number = NEW.item_number;
+            END IF;
+        END IF;
+        RETURN NEW;
+    END;
+    $$";
+        DB::unprepared($sql);
+        
+        DB::unprepared("CREATE TRIGGER verify_asset_number_update BEFORE UPDATE ON assets FOR EACH ROW EXECUTE PROCEDURE verify_asset_number();");
+        DB::unprepared("CREATE TRIGGER verify_asset_number_insert BEFORE INSERT ON assets FOR EACH ROW EXECUTE PROCEDURE verify_asset_number();");
+    }
+
+    public function down()
+    {
+          DB::unprepared('DROP FUNCTION IF EXISTS verify_asset_number() CASCADE;');
+    }
+}
